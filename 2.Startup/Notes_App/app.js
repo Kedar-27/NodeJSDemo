dc2f04b15739efbcd4ac4e41a5ce81fc27a7acb4
Kedar Sukerkar <kedarjsukerkar@yahoo.co.in>
2020-01-20 01:52:45 +0530

    Yargs package installed

--- 2.Startup/Notes_App/app.js
+++ 2.Startup/Notes_App/app.js
@@ -1,3 +1,11 @@
+const chalk = require('chalk')
 const  notes = require('./notes.js')
 
 console.log(notes())
+
+
+console.log(chalk.green(
+    'I am a green line ' +
+    chalk.blue.underle.inverse.bold('with a blue substring') +
+    ' that becomes green again!'
+))
