ce79ff5e831fbce34907e43234bc4e510d830696
Kedar Sukerkar <kedarjsukerkar@yahoo.co.in>
2020-01-21 00:38:16 +0530

    Yargs commands added

--- 2.Startup/Notes_App/app.js
+++ 2.Startup/Notes_App/app.js
@@ -1,11 +1,53 @@
 const chalk = require('chalk')
-const  notes = require('./notes.js')
+const yargs = require('yargs')
+const getNotes = require('./notes.js')
 
-console.log(notes())
+// Customize yargs version
+yargs.version('1.1.0')
+
+// Create add command
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    builder:{
+        title:{
+            describe: 'Add new note',
+            demandOption: true,
+            type: 'string'
+        }
+    } ,
+    handler: function (argv) {
+        console.log('Adding a new note!', argv.title)
+    }
+})
+
+// Create remove command
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a note',
+    handler: function () {
+        console.log('Removing the note')
+    }
+})
+
+// Create list command
+yargs.command({
+    command: 'list',
+    describe: 'List your notes',
+    handler: function () {
+        console.log('Listing out all notes')
+    }
+})
+
+// Create read command
+yargs.command({
+    command: 'read',
+    describe: 'Read a note',
+    handler: function () {
+        console.log('Reading a note')
+    }
+})
 
 
-console.log(chalk.green(
-    'I am a green line ' +
-    chalk.blue.underle.inverse.bold('with a blue substring') +
-    ' that becomes green again!'
-))
+//console.log(yargs.argv) can also be writte as
+yargs.parse()
