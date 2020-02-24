957f3ca67149d10d7aa000252daa3d0edf4ea788
Kedar27 <kedarjsukerkar@yahoo.co.in>
2020-02-25 02:15:57 +0530

    Hbs module added, web-server improved using html,css

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,29 +1,43 @@
+const path = require('path')
 const express = require('express')
 
 let app = express()
 
 
-app.get('', (request, response) => {
-
-    response.send('Hello, Express')
+app.set('view engine', 'hbs');
 
 
-})
+// Removed static route handler
 
-app.get('/help', (request, response) => {
+// app.get('', (request, response) => {
 
-    response.send('Happy to help')
-})
+//     response.send('<h1>Hello Express</>')
 
 
-app.get('/about', (request, response) => {
+// })
 
-    response.send('About')
-})
+// app.get('/help', (request, response) => {
+
+//     response.send([{name: 'Ked27', age: 30}])
+// })
+
+
+// app.get('/about', (request, response) => {
+
+//     response.send('<h2>About </>')
+// })
+
+
+const publicDirectoryPath = path.join(__dirname,'../public')
+
+
+app.use(express.static(publicDirectoryPath))
+//app.use(express.static('/help',publicDirectoryPath))
+
 
 app.get('/weather', (request, response) => {
 
-    response.send('Weather is loading')
+    response.send({location: 'India', forecast: '50 degress'})
 })
 
 
